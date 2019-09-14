5078996b09be7b72753a1c9d15c8eee07bc02928
Cesar Andres Ramirez <candresramirez@gmail.com>
2019-09-14 10:50:33 -0500

    proyecto crud rest codeigniter

---
+++ application/controllers/Api.php
@@ -0,0 +1,129 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Api extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('api_model');
+		$this->load->library('form_validation');
+	}
+
+	function index()
+	{
+		$data = $this->api_model->fetch_all();
+		echo json_encode($data->result_array());
+	}
+
+	function insert()
+	{
+		$this->form_validation->set_rules('first_name', 'Nombre', 'required');
+		$this->form_validation->set_rules('email', 'Correo', 'required|valid_email');
+		$this->form_validation->set_rules('cellphone', 'Celular', 'required');
+		$this->form_validation->set_rules('visit_reason', 'Motivo Visita', 'required');
+		if($this->form_validation->run())
+		{
+			$data = array(
+				'first_name'	=>	$this->input->post('first_name'),
+				'email'		 	=>	$this->input->post('email'),
+				'cellphone'		=>	$this->input->post('cellphone'),
+				'visit_reason'	=>	$this->input->post('visit_reason'),
+				'commentary'	=>	$this->input->post('commentary')
+			);
+
+			$this->api_model->insert_api($data);
+
+			$array = array(
+				'success'		=>	true
+			);
+		}
+		else
+		{
+			$array = array(
+				'error'					=>	true,
+				'first_name_error'		=>	form_error('first_name'),
+				'email_error'			=>	form_error('email'),
+				'cellphone_error'		=>	form_error('cellphone'),
+				'visit_reason_error'	=>	form_error('visit_reason')
+			);
+		}
+		echo json_encode($array);
+	}
+	
+	function fetch_single()
+	{
+		if($this->input->post('id'))
+		{
+			$data = $this->api_model->fetch_single_user($this->input->post('id'));
+
+			foreach($data as $row)
+			{
+				$output['first_name'] = $row['first_name'];
+				$output['email'] = $row['email'];
+				$output['visit_reason'] = $row['visit_reason'];
+			}
+			echo json_encode($output);
+		}
+	}
+
+	function update()
+	{
+		$this->form_validation->set_rules('first_name', 'First Name', 'required');
+		$this->form_validation->set_rules('email', 'Correo', 'required');
+		$this->form_validation->set_rules('cellphone', 'Celular', 'required');
+		$this->form_validation->set_rules('visit_reason', 'Motivo Visita', 'required');
+		if($this->form_validation->run())
+		{	
+			$data = array(
+				'first_name'		=>	$this->input->post('first_name'),
+				'email'		 	=>	$this->input->post('email'),
+				'cellphone'		=>	$this->input->post('cellphone'),
+				'visit_reason'	=>	$this->input->post('visit_reason'),
+				'commentary'	=>	$this->input->post('commentary')
+			);
+
+			$this->api_model->update_api($this->input->post('id'), $data);
+
+			$array = array(
+				'success'		=>	true
+			);
+		}
+		else
+		{
+			$array = array(
+				'error'				=>	ture,
+				'first_name_error'	=>	form_error('first_name'),
+				'email_error'			=>	form_error('email'),
+				'cellphone_error'		=>	form_error('cellphone'),
+				'visit_reason_error'	=>	form_error('visit_reason')
+			);
+		}
+		echo json_encode($array);
+	}
+
+	function delete()
+	{
+		if($this->input->post('id'))
+		{
+			if($this->api_model->delete_single_user($this->input->post('id')))
+			{
+				$array = array(
+
+					'success'	=>	true
+				);
+			}
+			else
+			{
+				$array = array(
+					'error'		=>	true
+				);
+			}
+			echo json_encode($array);
+		}
+	}
+
+}
+
+
+?>
---
+++ application/views/api_view.php
@@ -0,0 +1,192 @@
+<html>
+<head>
+    <title>CURD-REST</title>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    
+</head>
+<body>
+    <div class="container">
+        <br />
+        <h3 align="center">CRUD REST</h3>
+        <br />
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h3 class="panel-title">CRUD REST By Cesar Ramirez</h3>
+                    </div>
+                    <div class="col-md-6" align="right">
+                        <button type="button" id="add_button" class="btn btn-info btn-xs">Add</button>
+                    </div>
+                </div>
+            </div>
+            <div class="panel-body">
+                <span id="success_message"></span>
+                <table class="table table-bordered table-striped">
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Correo</th>
+                            <th>Motivo de visita</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
+<div id="userModal" class="modal fade">
+    <div class="modal-dialog">
+        <form method="post" id="user_form">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title">Add User</h4>
+                </div>
+                <div class="modal-body">
+                    <label>Nombre</label>
+                    <input type="text" name="first_name" id="first_name" class="form-control" />
+                    <span id="first_name_error" class="text-danger"></span>
+                    <br />
+                    <label>Correo</label>
+                    <input type="text" name="email" id="email" class="form-control" />
+                    <span id="email_error" class="text-danger"></span>
+                    <br />
+                    <label>Celular</label>
+                    <input type="text" name="cellphone" id="cellphone" class="form-control" />
+                    <span id="cellphone_error" class="text-danger"></span>
+                    <br />
+                    <label>Motivo de visita</label>                 
+                    <select name="visit_reason" id="visit_reason" class="form-control">
+                            <option value="compra">Compra</option>
+                            <option value="venta">Venta</option>
+                            <option value="alquiler" selected="selected">Alquiler</option>
+                    </select>
+                    <span id="visit_reason_error" class="text-danger"></span>
+                    <br />
+                    <label>Comentario</label>
+                    <textarea rows = "5" cols = "50" name="commentary" id="commentary" class="form-control"></textarea>
+                    <span id="commentary_error" class="text-danger"></span>
+                    <br />
+                </div>
+                <div class="modal-footer">
+                    <input type="hidden" name="user_id" id="user_id" />
+                    <input type="hidden" name="data_action" id="data_action" value="Insert" />
+                    <input type="submit" name="action" id="action" class="btn btn-success" value="Add" />
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+
+<script type="text/javascript" language="javascript" >
+$(document).ready(function(){
+    
+    function fetch_data()
+    {
+        $.ajax({
+            url:"<?php echo base_url(); ?>test_api/action",
+            method:"POST",
+            data:{data_action:'fetch_all'},
+            success:function(data)
+            {
+                $('tbody').html(data);
+            }
+        });
+    }
+
+    fetch_data();
+
+    $('#add_button').click(function(){
+        $('#user_form')[0].reset();
+        $('.modal-title').text("Add User");
+        $('#action').val('Add');
+        $('#data_action').val("Insert");
+        $('#userModal').modal('show');
+    });
+
+    $(document).on('submit', '#user_form', function(event){
+        event.preventDefault();
+        $.ajax({
+            url:"<?php echo base_url() . 'test_api/action' ?>",
+            method:"POST",
+            data:$(this).serialize(),
+            dataType:"json",
+            success:function(data)
+            {
+                if(data.success)
+                {
+                    $('#user_form')[0].reset();
+                    $('#userModal').modal('hide');
+                    fetch_data();
+                    if($('#data_action').val() == "Insert")
+                    {
+                        $('#success_message').html('<div class="alert alert-success">Data Inserted</div>');
+                    }
+                }
+
+                if(data.error)
+                {
+                    $('#first_name_error').html(data.first_name_error);
+                    $('#email_error').html(data.email_error);
+                    $('#cellphone_error').html(data.cellphone_error);
+                    $('#visit_reason_error').html(data.visit_reason_error);
+                }
+            }
+        })
+    });
+
+    $(document).on('click', '.edit', function(){
+        var user_id = $(this).attr('id');
+        $.ajax({
+            url:"<?php echo base_url(); ?>test_api/action",
+            method:"POST",
+            data:{user_id:user_id, data_action:'fetch_single'},
+            dataType:"json",
+            success:function(data)
+            {
+                $('#userModal').modal('show');
+                $('#first_name').val(data.first_name);
+                $('#last_name').val(data.last_name);
+                $('.modal-title').text('Edit User');
+                $('#user_id').val(user_id);
+                $('#action').val('Edit');
+                $('#data_action').val('Edit');
+            }
+        })
+    });
+
+    $(document).on('click', '.delete', function(){
+        var user_id = $(this).attr('id');
+        if(confirm("Esta seguro de Borrar?"))
+        {
+            $.ajax({
+                url:"<?php echo base_url(); ?>test_api/action",
+                method:"POST",
+                data:{user_id:user_id, data_action:'Delete'},
+                dataType:"JSON",
+                success:function(data)
+                {
+                    if(data.success)
+                    {
+                        $('#success_message').html('<div class="alert alert-success">Data Deleted</div>');
+                        fetch_data();
+                    }
+                }
+            })
+        }
+    });
+    
+});
+</script>
